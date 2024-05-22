c9e5a417a8f8112501bb0c1716fc36b83abf0267
Muhammad Zaeem Khan <74709791+zaeemkhan1999@users.noreply.github.com>
2024-05-23 04:00:23 +0500

    file upload to root done

--- frontend/src/pages/api/upload.js
+++ frontend/src/pages/api/upload.js
@@ -1,12 +1,10 @@
-// pages/api/upload.js
 import multer from 'multer';
 import path from 'path';
 import { promisify } from 'util';
 import fs from 'fs';
 
-// Create the uploads directory if it doesn't exist
 const mkdir = promisify(fs.mkdir);
-const uploadDir = path.join(process.cwd(), 'uploads');
+const uploadDir = path.resolve(process.cwd(), '../uploads');
 
 async function ensureUploadDir() {
   try {
@@ -18,13 +16,12 @@ async function ensureUploadDir() {
   }
 }
 
-// Configure multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, 'data' + path.extname(file.originalname));
   },
 });
 
@@ -41,14 +38,11 @@ const handler = async (req, res) => {
 
   upload.single('file')(req, res, (err) => {
     if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
       return res.status(500).json({ error: err.message });
     } else if (err) {
-      // An unknown error occurred when uploading.
       return res.status(500).json({ error: err.message });
     }
 
-    // Everything went fine.
     res.status(200).json({ data: 'File uploaded successfully' });
   });
 };
